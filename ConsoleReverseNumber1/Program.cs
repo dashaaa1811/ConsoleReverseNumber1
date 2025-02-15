8aca649e7ce0151b9ae535c91ad2d9c36d93081a
Denis <Denis@HOME-PC>
2025-02-15 12:52:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleReverseNumber1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleReverseNumber1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(ReverseNumber(num));
+        }
+        static int ReverseNumber(int num)
+        {
+            int reverse = 0;
+            while (num > 0)
+            {
+                int last = num % 10;
+                reverse=reverse*10 + last;
+                num /= 10;
+            }
+            return reverse;
+        }
+
+
+    }
+}
